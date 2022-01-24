a3ca65c14ea3f9ac15a6c72e9ab621d971e6a271
luis(casa) <lperezhurtado97@gmail.com>
2022-01-25 00:16:33 +0100

    AGENDA DE CONTACTOS ACTUALIZADA EN CASA

--- CasosPracticos/src/POO_AgendaContacto/Agenda.java
+++ CasosPracticos/src/POO_AgendaContacto/Agenda.java
@@ -6,8 +6,45 @@ public class Agenda {
     private Contacto[] agenda = new Contacto[100];
     private int cuantos = 0;
     
-    public void setNombre(String nombre){
-        
+    //Agregar contacto
+    public void agregarContacto(String n, String t, String c){
+        if(cuantos < 100){
+            agenda[cuantos].setNombre(n);
+            agenda[cuantos].setTelefono(t);
+            agenda[cuantos].setCorreo(c);
+            
+            cuantos++;
+        }
+        else{
+            System.out.println("Error: No se pueden añadir mas contactos");
+        }
     }
+    
+    //Borrar contacto
+    public void borrarContacto(int pos){
+        for (int i = pos; i < (agenda.length-1); i++) {
+            agenda[i] = agenda[i+1];
+        }
+        cuantos--;
+    }
+    
+    public void buscarContacto(String nombre){
+        int pos = 0;
+        for (int i = 0; i < agenda.length; i++) { 
+            if(agenda[i].getNombre().toUpperCase() == nombre.toUpperCase()){
+                pos = i;
+            }
+        }
+         agenda[pos].imprime();
+    }
+    
+    //Imprimir todos los contactos
+    public void imprimirTodos(){
+        for (int i = 0; i < agenda.length; i++) {
+            agenda[i].imprime();   
+        }
+    }
+    
+    
 
 }
