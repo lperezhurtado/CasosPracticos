08cd0081bb3ed8e35f7b396645e763fa875a8b97
luis(casa) <lperezhurtado97@gmail.com>
2022-01-25 23:04:49 +0100

    AGENDA DE CONTACTOS ACTUALIZADA EN CASA (actualizada)

--- CasosPracticos/src/POO_AgendaContacto/Agenda.java
+++ CasosPracticos/src/POO_AgendaContacto/Agenda.java
@@ -1,11 +1,23 @@
 
 package POO_AgendaContacto;
 
+import java.util.Scanner;
+
 public class Agenda {
     
     private Contacto[] agenda = new Contacto[100];
     private int cuantos = 0;
     
+    //menu
+    public static void menu(){
+        System.out.println("MENU \n"
+                + "1. Ver contactos \n"
+                + "2. Agregar contactos \n"
+                + "3. Eliminar contacto \n"
+                + "4. Buscar por nombre \n"
+                + "5. Salir \n"
+                + "ELIGE UNA OPCION");
+    }
     //Agregar contacto
     public void agregarContacto(String n, String t, String c){
         if(cuantos < 100){
@@ -43,6 +55,11 @@ public class Agenda {
         for (int i = 0; i < agenda.length; i++) {
             agenda[i].imprime();   
         }
+    }
+    
+    public String pedirDatos(){
+        Scanner lector = new Scanner(System.in);
+        return lector.nextLine();
     }
     
     
---
+++ CasosPracticos/src/POO_AgendaContacto/Main.java
@@ -0,0 +1,15 @@
+
+package POO_AgendaContacto;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        do {
+            Agenda.menu();
+            
+            
+        } while (true);
+        
+    }
+}
